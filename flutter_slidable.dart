28a13c9bea225b17132a1420fb39258d2e45719e
Hiren Ramani <59291043+Hero-9500@users.noreply.github.com>
2021-05-10 18:29:57 +0530

    Add files via upload

---
+++ flutter_slidable.dart
@@ -0,0 +1,9 @@
+library flutter_slidable;
+
+export 'package:jkr/library/slidable/src/widgets/slidable.dart';
+export 'package:jkr/library/slidable/src/widgets/slide_action.dart';
+export 'package:jkr/library/slidable/src/widgets/slidable_action_pane.dart';
+export 'package:jkr/library/slidable/src/widgets/slidable_dismissal.dart';
+
+export 'package:jkr/library/slidable/src/widgets/fractionnally_aligned_sized_box.dart';
+
